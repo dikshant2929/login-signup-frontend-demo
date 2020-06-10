5a04e633bf3a7fbdcb63dc6487ca2a13f43e2a16
Dikshant Godara <dikshant.godara@girnarsoft.co.in>
2020-06-10 20:24:52 +0530

    Build Release

---
+++ build/precache-manifest.3d6c000acdff0054a28a3ad6fc9546fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8543bf4f247b770e7146a0ac465d67a0",
+    "url": "/login-signup-frontend-demo/index.html"
+  },
+  {
+    "revision": "d5b7619ba4e3c5f02ca3",
+    "url": "/login-signup-frontend-demo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1563ed8a1035f5130590",
+    "url": "/login-signup-frontend-demo/static/css/main.5562233c.chunk.css"
+  },
+  {
+    "revision": "d5b7619ba4e3c5f02ca3",
+    "url": "/login-signup-frontend-demo/static/js/2.15f001ae.chunk.js"
+  },
+  {
+    "revision": "b09d35101ec0b3e28e217c6b6e9e49f3",
+    "url": "/login-signup-frontend-demo/static/js/2.15f001ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1563ed8a1035f5130590",
+    "url": "/login-signup-frontend-demo/static/js/main.9c0ee623.chunk.js"
+  },
+  {
+    "revision": "cf925e5809359af101bc",
+    "url": "/login-signup-frontend-demo/static/js/runtime-main.991e3f81.js"
+  }
+]);
